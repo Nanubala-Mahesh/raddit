8ba3163464e12a0d6b57b57fb24f3c12e0729ec8
Mahesh <mahesh.bablu17@gmail.com>
2016-08-22 23:45:48 +0530

    association b/w Link and User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822173259) do
+ActiveRecord::Schema.define(version: 20160822181215) do
 
   create_table "links", force: true do |t|
     t.string   "title"
